6c2762f2df09738dd677cbf153725457f05b4746
Hackjaku <stressfest@rocketmail.com>
2022-05-07 19:40:11 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+List<string> recuperi = new List<string>();
+List<string> liberazioni = new List<string>();
+List<string> animali = new List<string>();
+
+
+
+namespace ExcelCombiner {
+    public static class Utilities {
+        public static List<string> GetCSV(string filePath) {
+            var result = new List<string>();
+            using var reader = new StreamReader(filePath);
+            while(!reader.EndOfStream) {
+                // leggi il file
+                result.Add(reader.ReadLine()!);
+            }
+            return result;
+        }
+
+    }
+
+    public class Trip {
+        int Progressive { get; set; }
+        double Km { get; set; }
+
+    }
+
+    public class LiberationTrip : Trip {
+        
+    }
+
+}
